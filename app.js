5bec6406d61612a6d175ee48c1c46409d6bfc00d
phish <phishchiang@gmail.com>
2020-05-17 15:49:03 +0800

    add back the express.json

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post('/', line.middleware(config), (req, res) => {
     });
 });
 
-// app.use(express.json());
+app.use(express.json());
 app.use('/api/v1/guessState', guessState);
 
 let magicNum = 0;
